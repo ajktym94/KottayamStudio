71d649920206028839392c2d28e0de51511253f4
Ajay Mathew Joseph <ajay94.mec@gmail.com>
2024-11-03 01:03:36 +0100

    Removed code for heroku env setup

--- utils.py
+++ utils.py
@@ -42,8 +42,6 @@ def set_heroku_env_variable(app_name, key, value):
 creds = None
 # The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
 creds = Credentials.from_authorized_user_info(token_json)
-# if os.path.exists("token.json"):
-#     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
@@ -53,15 +51,8 @@ if not creds or not creds.valid:
         flow = InstalledAppFlow.from_client_config(
             credentials_json, SCOPES
         )
-        # flow = InstalledAppFlow.from_client_secrets_file(
-        #     "credentials.json", SCOPES
-        # )
-        creds = flow.run_local_server(port=0)
 
-    # Save the credentials for the next run
-    set_heroku_env_variable('kottayamstudio', 'GDRIVE_TOKEN', creds.to_json())
-    # with open("token.json", "w") as token:
-    #     token.write(creds.to_json())
+        creds = flow.run_local_server(port=0)
 
 service = build('drive', 'v3', credentials=creds)
 
